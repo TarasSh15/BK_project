1c91473c636034f7041f46d20bf49b82b3deb1ad
TarasSh <tarassh15@gmail.com>
2023-04-29 20:53:15 +0300

    Start0.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,59 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.22.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
-provider aws {
-    region = "eu-central-1"
+provider "aws" {
+  region  = "eu-central-1"
 }
 
-resource "aws_instance" "Jenkins" {
-  ami                         = "ami-0c9354388bb36c088"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  key_name                    = "mypersonalkey"
-  vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fec2c2e2017f4e7b"
+  instance_type = "t2.micro"
+
   tags = {
-    Name = "Jenkins"
+    Name = "Debian_main"
   }
 }
 
-resource "aws_instance" "Apache" {
-  ami                         = "ami-0c9354388bb36c088"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  key_name                    = "mypersonalkey"
-  vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
-  tags = {
-    Name = "Apache"
-  }
-}
+# resource "aws_instance" "lb" {
+#   ami                         = "ami-0c9354388bb36c088"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   key_name                    = "mypersonalkey"
+#   vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
+#   tags = {
+#     Name = "LoadBalancer"
+#   }
+# }
+
+# resource "aws_instance" "Jenkins" {
+#   ami                         = "ami-0c9354388bb36c088"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   key_name                    = "mypersonalkey"
+#   vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
+#   tags = {
+#     Name = "Apache"
+#   }
+# }
+
+# resource "aws_instance" "Project" {
+#   ami                         = "ami-0c9354388bb36c088"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   key_name                    = "mypersonalkey"
+#   vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
+#   tags = {
+#     Name = "Apache"
+#   }
+# }
 # resource "aws_security_group" "web" {
 #   name = "Jenkins-sg"
 #   ingress {
