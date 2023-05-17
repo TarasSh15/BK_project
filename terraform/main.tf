ac84b13e60eca48a2e88801f4b66938f940fcbc0
TarasSh <tarassh15@gmail.com>
2023-05-17 14:51:30 +0300

    reliease

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,23 +23,33 @@ resource "aws_instance" "lb" {
   }
 }
 
-resource "aws_instance" "Jenkins" {
+resource "aws_instance" "Jenkins-sql" {
   ami                      = "ami-0ec7f9846da6b0f61"
   instance_type            = "t2.micro"
   key_name                 = "aws_key"
-  vpc_security_group_ids   = ["sg-0da045203b980776d"]
+  vpc_security_group_ids   = ["sg-0015baf4924c6430d"]
   tags = {
-    Name = "Jenkins"
+    Name = "Jenkins-sql"
   }
 }
 
-resource "aws_instance" "WebApp" {
+resource "aws_instance" "WebApp1" {
   ami                      = "ami-0ec7f9846da6b0f61"
   instance_type            = "t2.micro"
   key_name                 = "aws_key"
-  vpc_security_group_ids   = ["sg-0da045203b980776d"]
+  vpc_security_group_ids   = ["sg-0d94338c1696a6cc0"]
   tags = {
-    Name = "WebApp"
+    Name = "WebApp1"
+  }
+}
+
+resource "aws_instance" "WebApp2" {
+  ami                      = "ami-0ec7f9846da6b0f61"
+  instance_type            = "t2.micro"
+  key_name                 = "aws_key"
+  vpc_security_group_ids   = ["sg-0d94338c1696a6cc0"]
+  tags = {
+    Name = "WebApp2"
   }
 }
 
