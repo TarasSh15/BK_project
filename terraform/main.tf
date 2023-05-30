2e4a51ff033e5f3e9658372faab8fd6f9aa633e5
TarasSh <tarassh15@gmail.com>
2023-05-30 16:40:34 +0300

    release

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,23 +13,23 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_instance" "lb" {
+resource "aws_instance" "Lb-jenkins" {
   ami                      = "ami-0ec7f9846da6b0f61"
   instance_type            = "t2.micro"
   key_name                 = "aws_key"
   vpc_security_group_ids   = ["sg-0da045203b980776d"]
   tags = {
-    Name = "LoadBalancer"
+    Name = "lb-jenkins"
   }
 }
 
-resource "aws_instance" "Jenkins-sql" {
+resource "aws_instance" "Sql-server" {
   ami                      = "ami-0ec7f9846da6b0f61"
   instance_type            = "t2.micro"
   key_name                 = "aws_key"
-  vpc_security_group_ids   = ["sg-0015baf4924c6430d"]
+  vpc_security_group_ids   = ["sg-09ca1f8b5838a24fe"]
   tags = {
-    Name = "Jenkins-sql"
+    Name = "Sql-server"
   }
 }
 
@@ -52,64 +52,3 @@ resource "aws_instance" "WebApp2" {
     Name = "WebApp2"
   }
 }
-
-# resource "aws_s3_bucket" "dbdump" {
-#   bucket = "tf-mysql-bucket-forapp"
-
-#   tags = {
-#     Name        = "tf-mysql-bucket-forapp"
-#     Environment = "Dev"
-#   }
-# }
-
-# resource "aws_s3_bucket_object" "s3dump" {
-#   bucket = aws_s3_bucket.dbdump.id
-#   for_each = fileset("./dumps/", "**")
-#   key = each.value
-#   source = "./dumps/${each.value}"
-#   etag = filemd5("./dumps/${each.value}")
-# }
-
-# resource "aws_db_instance" "display" {
-#   allocated_storage    = 10
-#   db_name              = "display"
-#   engine               = "mysql"
-#   engine_version       = "8.0.32"
-#   instance_class       = "db.t3.micro"
-#   username             = "root"
-#   password             = "123321"
-#   parameter_group_name = "default.mysql8.0.32"
-#   skip_final_snapshot  = true
-# }
-
-#-----------------------------------------------------------
-# resource "aws_instance" "Jenkins" {
-#   ami                         = "ami-0c9354388bb36c088"
-#   instance_type               = "t2.micro"
-#   associate_public_ip_address = true
-#   key_name                    = "mypersonalkey"
-#   vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
-#   tags = {
-#     Name = "Apache"
-#   }
-# }
-
-# resource "aws_instance" "Project" {
-#   ami                         = "ami-0c9354388bb36c088"
-#   instance_type               = "t2.micro"
-#   associate_public_ip_address = true
-#   key_name                    = "mypersonalkey"
-#   vpc_security_group_ids      = ["sg-01a682529a9cb3dc4"]
-#   tags = {
-#     Name = "Apache"
-#   }
-# }
-# resource "aws_security_group" "web" {
-#   name = "Jenkins-sg"
-#   ingress {
-#     from_port = 22
-#     to_port = 22
-#     protocol = "tcp"
-#     cidr_blocks = [ "91.243.26.110/32" ]
-#   }
-# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,36 +1,16 @@
-output "jenkins_public_ip" {
-  description = "Public Jenkins IP "
-  value       = aws_instance.Jenkins-sql.public_ip
-}
-
-output "jenkins_private_ip" {
-  description = "Private Jenkins IP "
-  value = aws_instance.Jenkins-sql.private_ip
-}
-
 output "lb_public_ip" {
   description = "Public LB IP "
-  value       = aws_instance.lb.public_ip
+  value       = aws_instance.Lb-jenkins.public_ip
 }
 
-output "lb_private_ip" {
-  description = "Private LB IP "
-  value = aws_instance.lb.private_ip
-}
-
-output "webapp1_public_ip" {
-  description = "Public WebApp IP "
-  value       = aws_instance.WebApp1.public_ip
+output "sql_private_ip" {
+  description = "Private Jenkins IP "
+  value = aws_instance.Sql-server.private_ip
 }
 
 output "webapp1_private_ip" {
   description = "Private WebApp IP "
   value = aws_instance.WebApp1.private_ip
-}
-
-output "webapp2_public_ip" {
-  description = "Public WebApp IP "
-  value       = aws_instance.WebApp2.public_ip
 }
 
 output "webapp2_private_ip" {
